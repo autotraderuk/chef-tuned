51b9411730aad12f853c4f7028fc640c32701034
david.meekin <david.meekin@autotrader.co.uk>
2015-11-18 10:09:19 +0000

    updated maintainer

--- test/cookbooks/test_tuned/metadata.rb
+++ test/cookbooks/test_tuned/metadata.rb
@@ -1,5 +1,5 @@
 name 'test_tuned'
-maintainer 'Auto Trader, UK.'
+maintainer 'Dave Meekin'
 maintainer_email 'davemeekin.git@gmail.com'
 license 'All rights reserved'
 description 'test cookbook to call tuned LWRP resources'
