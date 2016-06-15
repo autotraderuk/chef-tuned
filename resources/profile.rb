be74a840309d989d8ccb98cadd15fcd6870e6cf9
Jeremy MAURO <j.mauro@criteo.com>
2016-06-15 20:19:52 +0200

    Adding backward compattibility for chef < 12.5

--- resources/profile.rb
+++ resources/profile.rb
@@ -1,10 +1,20 @@
 actions :create, :enable, :disable, :default
 default_action :create
 
-# man 'tuned.conf'
-# MAIN SECTION
-#   The main section is called "[main]"
-property :main, kind_of: Hash, default: {}.freeze
-# PLUGINS
-#   Every other section defines one plugin.
-property :plugins, kind_of: Hash, default: {}.freeze
+if defined?(property)
+  # man 'tuned.conf'
+  # MAIN SECTION
+  #   The main section is called "[main]"
+  property :main, kind_of: Hash, default: {}.freeze
+  # PLUGINS
+  #   Every other section defines one plugin.
+  property :plugins, kind_of: Hash, default: {}.freeze
+else
+  # man 'tuned.conf'
+  # MAIN SECTION
+  #   The main section is called "[main]"
+  attribute :main, kind_of: Hash, default: {}.freeze
+  # PLUGINS
+  #   Every other section defines one plugin.
+  attribute :plugins, kind_of: Hash, default: {}.freeze
+end
